98d8245a6786f7712eedaebae6df616f3df1e12e
Hilmar Wiegand <me@hwgnd.de>
2022-11-16 19:51:56 +0100

    Switch to eslint_d

--- config/nvim/lua/plugins/null-ls.lua
+++ config/nvim/lua/plugins/null-ls.lua
@@ -3,6 +3,7 @@ local null = require "null-ls"
 null.setup {
   sources = {
     null.builtins.formatting.prettier,
-    null.builtins.diagnostics.eslint,
+    null.builtins.diagnostics.eslint_d,
+    null.builtins.code_actions.eslint_d,
   }
 }
