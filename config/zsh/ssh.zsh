c1421d23f1c6475482fd3406f6fb7e10b871d1d9
Hilmar Wiegand <me@hwgnd.de>
2023-03-30 00:47:21 +0200

    Improve ssh-agent startup

---
+++ config/zsh/ssh.zsh
@@ -0,0 +1,19 @@
+SSH_ENV="$HOME/.ssh/agent-environment"
+
+function start_agent {
+  ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+  chmod 600 "${SSH_ENV}"
+  . "${SSH_ENV}" > /dev/null
+  ssh-add "$HOME/.ssh/$(hostname).personal.id_ed25519" &> /dev/null
+}
+
+if [[ -f "${SSH_ENV}" ]]; then
+  . "${SSH_ENV}" > /dev/null
+
+  ps -ef | \
+    grep ${SSH_AGENT_PID} | \
+    grep ssh-agent$ > /dev/null || \
+    { start_agent } || true
+else
+  start_agent || true
+fi
