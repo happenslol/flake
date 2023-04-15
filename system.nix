e8956c15eab26cb5158ebce9d9aa707879f48402
Hilmar Wiegand <me@hwgnd.de>
2023-04-15 17:24:24 +0200

    Update flake

--- system.nix
+++ system.nix
@@ -34,7 +34,7 @@
       dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=hyprland
       systemctl --user start pipewire wireplumber xdg-desktop-portal-hyprland
       sleep 2
-      systemctl --user start xdg-desktop-portal 
+      systemctl --user start xdg-desktop-portal
     '';
   };
 
