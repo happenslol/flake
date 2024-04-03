28fa60d0a3938906a964836be4b30c8c78612cd1
Hilmar Wiegand <me@hwgnd.de>
2024-04-03 15:19:44 +0200

    kitty: Improve config and styling

--- config/nvim/lua/plugins/kitty.lua
+++ config/nvim/lua/plugins/kitty.lua
@@ -7,12 +7,28 @@ return {
     event = { "User KittyScrollbackLaunch" },
     config = function()
       require("kitty-scrollback").setup({
+        {
+          paste_window = {
+            hide_footer = true,
+            yank_register = "y",
+          },
+        },
         callbacks = {
           after_ready = vim.defer_fn(function()
+            -- TODO: Keybindings don't show up with names in which-key
             vim.keymap.set("n", "q", "<cmd>qa!<cr>", { buffer = true, silent = true })
+            vim.keymap.set("v", "y", function()
+              print(vim.v.register)
+              if vim.v.register == '+' then
+                return "y<cmd>qa!<cr>"
+              else
+                return "y"
+              end
+            end, { buffer = true, expr = true, noremap = true })
+
             vim.opt.signcolumn = "no"
-          end, 0)
-        }
+          end, 0),
+        },
       })
     end,
   },
