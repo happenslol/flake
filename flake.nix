acc9c4554e49f0636b144bb777efb0c4dd94af8a
Hilmar Wiegand <me@hwgnd.de>
2024-05-12 13:00:17 +0200

    nix: Update flake inputs

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     };
 
     hyprland = {
-      url = "github:hyprwm/Hyprland";
+      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
