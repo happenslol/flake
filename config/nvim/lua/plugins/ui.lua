0dcf094c5c9879e88dd69ac236123e449e413aba
Hilmar Wiegand <me@hwgnd.de>
2024-05-24 15:44:35 +0200

    neovim: Remove alternate ctrl-f bindings

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -55,30 +55,30 @@ return {
         end,
         desc = "Noice All",
       },
-      {
-        "<c-f>",
-        function()
-          if not require("noice.lsp").scroll(4) then
-            return "<c-f>"
-          end
-        end,
-        silent = true,
-        expr = true,
-        desc = "Scroll forward",
-        mode = { "i", "n", "s" },
-      },
-      {
-        "<c-b>",
-        function()
-          if not require("noice.lsp").scroll(-4) then
-            return "<c-b>"
-          end
-        end,
-        silent = true,
-        expr = true,
-        desc = "Scroll backward",
-        mode = { "i", "n", "s" },
-      },
+      -- {
+      --   "<c-f>",
+      --   function()
+      --     if not require("noice.lsp").scroll(4) then
+      --       return "<c-f>"
+      --     end
+      --   end,
+      --   silent = true,
+      --   expr = true,
+      --   desc = "Scroll forward",
+      --   mode = { "i", "n", "s" },
+      -- },
+      -- {
+      --   "<c-b>",
+      --   function()
+      --     if not require("noice.lsp").scroll(-4) then
+      --       return "<c-b>"
+      --     end
+      --   end,
+      --   silent = true,
+      --   expr = true,
+      --   desc = "Scroll backward",
+      --   mode = { "i", "n", "s" },
+      -- },
     },
   },
 
