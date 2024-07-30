8b853f8b8318198532c3f0501dc514b3214e8661
Hilmar Wiegand <me@hwgnd.de>
2024-07-30 09:59:00 +0200

    neovim: Update whichkey config

--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -5,7 +5,6 @@ return {
     opts = {
       marks = true,
       registers = true,
-      operators = {},
       show_help = false,
 
       plugins = {
@@ -22,30 +21,23 @@ return {
 
       layout = { align = "center" },
       disabled = { filetypes = { "neo-tree" } },
-
-      triggers_nowait = {},
-      triggers_blacklist = {
-        n = { "d", "c" },
-        i = { "j", "k", "q", "@" },
-        v = { "j", "k" },
-      },
     },
     config = function(_, opts)
       -- TODO: Get this to work
       -- Show registers when pressing remapped ' key
-      local registers_actions = require("which-key.plugins.registers").actions
-      table.insert(registers_actions, { trigger = "'", mode = "n" })
-      table.insert(registers_actions, { trigger = "'", mode = "v" })
+      -- local registers_actions = require("which-key.plugins.registers").actions
+      -- table.insert(registers_actions, { trigger = "'", mode = "n" })
+      -- table.insert(registers_actions, { trigger = "'", mode = "v" })
 
       local wk = require("which-key")
       wk.setup(opts)
-      wk.register({
-        ["<space>g"] = { name = "+git" },
-        ["<space>q"] = { name = "+quickfix" },
-        ["<space>w"] = { name = "+loclist" },
-        ["<space>l"] = { name = "+meta" },
-        ["<space>m"] = { name = "+messages" },
-        ["<space>r"] = { name = "+rename" },
+      wk.add({
+        { "<space>g", group = "git" },
+        { "<space>q", group = "quickfix" },
+        { "<space>w", group = "loclist" },
+        { "<space>l", group = "meta" },
+        { "<space>m", group = "messages" },
+        { "<space>r", group = "rename" },
       })
     end,
   },
