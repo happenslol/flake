fd5af4f4be71421d80594f48476a5f153078d0e2
Hilmar Wiegand <me@hwgnd.de>
2024-08-15 11:27:48 +0200

    neovim: Update theme

--- config/nvim/lua/plugins/colors.lua
+++ config/nvim/lua/plugins/colors.lua
@@ -1,15 +1,11 @@
 return {
   {
     "happenslol/materialnight.nvim",
-    config = true,
+    lazy = false,
+    priority = 1000,
+    config = {},
     init = function()
       vim.cmd.colorscheme("materialnight")
-
-      local icons = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-      for name, icon in pairs(icons) do
-        name = "DiagnosticSign" .. name
-        vim.fn.sign_define(name, { text = icon, texthl = name })
-      end
     end,
   },
 }
