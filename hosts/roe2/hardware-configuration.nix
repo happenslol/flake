8518b00a5e9ff0c42ad29efe311c17d506117d63
Hilmar Wiegand <me@hwgnd.de>
2025-01-27 00:03:58 +0100

    nix: Update roe2 config

--- hosts/roe2/hardware-configuration.nix
+++ hosts/roe2/hardware-configuration.nix
@@ -9,39 +9,35 @@
   ];
 
   boot.initrd.availableKernelModules = ["nvme" "ahci" "xhci_pci" "usbhid" "usb_storage" "sd_mod"];
-  boot.initrd.kernelModules = [];
+  boot.initrd.kernelModules = ["zfs"];
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
 
-  fileSystems."/" = {
-    device = "rpool/system/root";
-    fsType = "zfs";
-  };
+  fileSystems."/" =
+    { device = "rpool/root";
+      fsType = "zfs";
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/1E1B-45C9";
-    fsType = "vfat";
-  };
+  fileSystems."/var" =
+    { device = "rpool/var";
+      fsType = "zfs";
+    };
 
-  fileSystems."/nix" = {
-    device = "rpool/nix";
-    fsType = "zfs";
-  };
+  fileSystems."/nix" =
+    { device = "rpool/nix";
+      fsType = "zfs";
+    };
 
-  fileSystems."/var" = {
-    device = "rpool/system/var";
-    fsType = "zfs";
-  };
+  fileSystems."/home" =
+    { device = "rpool/home";
+      fsType = "zfs";
+    };
 
-  fileSystems."/home/happens" = {
-    device = "rpool/user/home";
-    fsType = "zfs";
-  };
-
-  fileSystems."/nosync" = {
-    device = "rpool/user/nosync";
-    fsType = "zfs";
-  };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/4464-7A23";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
 
   swapDevices = [];
 
