dd9370377069acd87d26e7cd7c233af9d67e34ca
Hilmar Wiegand <me@hwgnd.de>
2025-05-27 15:54:12 +0200

    neovim: Update rust-analyzer config

--- config/nvim/lua/plugins/rust.lua
+++ config/nvim/lua/plugins/rust.lua
@@ -17,17 +17,8 @@ return {
         default_settings = {
           ["rust-analyzer"] = {
             cargo = {
-              allFeatures = true,
-              loadOutDirsFromCheck = true,
-              buildScripts = { enable = true },
-            },
-            procMacro = {
-              enable = true,
-              -- ignored = {
-              --   ["async-trait"] = { "async_trait" },
-              --   ["napi-derive"] = { "napi" },
-              --   ["async-recursion"] = { "async_recursion" },
-              -- },
+              features = "all",
+              targetDir = true,
             },
 
             files = {
