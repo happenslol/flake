ee0d05ee469c02814190baeb07b88eb29e00d420
Hilmar Wiegand <me@hwgnd.de>
2025-07-10 17:45:15 +0200

    neovim: Update conform config

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -332,21 +332,7 @@ return {
       {
         "<leader>f",
         function()
-          require("conform").format({
-            async = true,
-            timeout_ms = 3000,
-            quiet = false,
-            lsp_format = "first",
-            filter = function(client)
-              return not vim.tbl_contains({
-                "vtsls",
-                "gopls",
-                "nil_ls",
-                "lua_ls",
-                "html",
-              }, client.name)
-            end,
-          })
+          require("conform").format()
         end,
         mode = { "n", "v" },
         desc = "Format current buffer",
@@ -362,6 +348,7 @@ return {
         "graphql",
         "handlebars",
         "html",
+        "htmldjango",
         "javascript",
         "javascriptreact",
         "json",
@@ -376,34 +363,88 @@ return {
         "yaml",
       }
 
+      local biome_fts = {
+        "css",
+        "graphql",
+        "javascript",
+        "javascriptreact",
+        "json",
+        "jsonc",
+        "typescript",
+        "typescriptreact",
+      }
+
       local formatters_by_ft = {
         lua = { "stylua" },
-        fish = { "fish_indent" },
         sh = { "shfmt" },
         go = { "goimports", "gofumpt" },
         nix = { "alejandra" },
       }
 
-      for _, ft in ipairs(prettier_fts) do
-        if not formatters_by_ft[ft] then
-          formatters_by_ft[ft] = {}
+      local function insert_formatters(fts, formatter)
+        for _, ft in ipairs(fts) do
+          if not formatters_by_ft[ft] then
+            formatters_by_ft[ft] = {}
+          end
+          table.insert(formatters_by_ft[ft], formatter)
         end
-
-        table.insert(formatters_by_ft[ft], "prettierd")
       end
 
+      insert_formatters(biome_fts, "biome")
+      insert_formatters(prettier_fts, "prettierd")
+
+      ---@type table<string, table<string, boolean>>
+      local condition_cache = {}
+      local function cached(formatter, fn)
+        return function(self, ctx)
+          if condition_cache[formatter] == nil then
+            condition_cache[formatter] = {}
+          end
+
+          if condition_cache[formatter][ctx.filename] == nil then
+            condition_cache[formatter][ctx.filename] = fn(self, ctx)
+          end
+
+          return condition_cache[formatter][ctx.filename]
+        end
+      end
+
+      ---@type conform.setupOpts
       return {
+        default_format_opts = {
+          async = true,
+          timeout_ms = 3000,
+          quiet = false,
+
+          -- Only use lsp formatter for eslint
+          lsp_format = "first",
+          filter = function(client)
+            return vim.tbl_contains({ "eslint" }, client.name)
+          end,
+        },
         formatters_by_ft = formatters_by_ft,
         formatters = {
-          prettier = {
-            condition = function(_, ctx)
+          -- Only use biome if there's no prettier config
+          biome = {
+            condition = cached("biome", function(_, ctx)
+              return not util.formatting.has_prettier_config(ctx)
+            end),
+          },
+          prettierd = {
+            condition = cached("prettierd", function(_, ctx)
+              -- If we don't have biome, always use prettier
+              if not vim.tbl_contains(biome_fts, vim.bo.filetype) then
+                return true
+              end
+
+              -- If we have biome and no specific prettier config, use biome
               return util.formatting.has_prettier_config(ctx)
-            end,
+            end),
           },
           eslint_d = {
-            condition = function(_, ctx)
+            condition = cached("eslint_d", function(_, ctx)
               return util.formatting.has_eslint_config(ctx)
-            end,
+            end),
           },
           injected = { options = { ignore_errors = true } },
         },
@@ -412,14 +453,8 @@ return {
   },
 
   {
-    "nvimtools/none-ls.nvim",
-    event = "VeryLazy",
-    opts = { sources = {} },
-  },
-
-  {
     "Saecki/crates.nvim",
-    -- event = { "BufRead Cargo.toml" },
+    event = { "BufRead Cargo.toml" },
     opts = {
       completion = { crates = { enabled = true } },
       lsp = {
