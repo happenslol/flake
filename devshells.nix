2ada523022a13fa2c08cfeb040c1b198bd1677f7
Hilmar Wiegand <me@hwgnd.de>
2025-11-03 19:12:43 +0100

    sigma: Update devshell

--- devshells.nix
+++ devshells.nix
@@ -2,7 +2,7 @@
 pkgs: {
   sigma = pkgs.mkShell {
     name = "sigma";
-    packages = with pkgs; [fnm pkgs.playwright watchman];
+    packages = with pkgs; [fnm playwright watchman];
     shellHook = ''
       export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
       export PLAYWRIGHT_BROWSERS_PATH="${pkgs.playwright-driver.browsers}"
