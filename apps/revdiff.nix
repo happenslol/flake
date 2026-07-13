f88054f8e8dd48d7b1a7569d61d4bbd8bae1f320
Hilmar Wiegand <me@hwgnd.de>
2026-07-14 00:29:26 +0200

    revdiff: update to 1.11.1

--- apps/revdiff.nix
+++ apps/revdiff.nix
@@ -6,7 +6,7 @@
 }:
 stdenv.mkDerivation rec {
   pname = "revdiff";
-  version = "1.3.0";
+  version = "1.11.1";
   sourceRoot = ".";
   nativeBuildInputs = [autoPatchelfHook];
   installPhase = ''
@@ -14,6 +14,6 @@ stdenv.mkDerivation rec {
   '';
   src = fetchurl {
     url = "https://github.com/umputun/revdiff/releases/download/v${version}/revdiff_${version}_linux_amd64.tar.gz";
-    hash = "sha256-XM29vRhL7mlyGdwtDDpS5vRiCc8lyhAofBgBTz8Nm9g=";
+    hash = "sha256-eVimvvcjJn/tGLC+lkdrt2djav6WYzjtfjcMClBv1Uw=";
   };
 }
